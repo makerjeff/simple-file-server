e087c15737e1ab1e55fb5aea08d94480941b4c4c
Jeff Wu <makerjeffwu@gmail.com>
2017-03-16 11:49:26 -0700

    updated

--- client.py
+++ client.py
@@ -5,7 +5,10 @@ import os
 from custom_colors import bcolors
 
 # Global Variables
-tool_select_string = bcolors.OKGREEN + '1) list files, 2) download file, Q) quit-> ' + bcolors.ENDC
+tool_select_string = bcolors.OKGREEN + '1) list files, 2) download file, q) quit-> ' + bcolors.ENDC
+
+def get_main():
+
 
 
 def Main():
@@ -18,7 +21,7 @@ def Main():
         print 'Your selection: ' + message
         message = raw_input(tool_select_string)
 
-    print bcolors.WARNING + 'Terminating program.' + bcolors.ENDC
+    print bcolors.OKBLUE + 'Terminating program.' + bcolors.ENDC
     sys.exit(0)
 
 
